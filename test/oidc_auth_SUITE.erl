215845fbb8f1be8561ec2a5b07f7c3b40a0c1204
Minh Nguyễn <minhnv.vnu@gmail.com>
2026-03-17 04:03:48 +0700

    Create test suite for OIDC authentication

---
+++ test/oidc_auth_SUITE.erl
@@ -0,0 +1,18 @@
+%% OIDC Authentication Test Suite
+
+-module(oidc_auth_SUITE).
+
+-export([suite/0]).
+
+-suite() ->
+    [
+        %% Add your test cases here
+        % test_function_name(),
+        %% Test case examples
+    ].
+
+%% Example test case
+%
+test_function_name() ->
+%    %% Add test logic here
+%
